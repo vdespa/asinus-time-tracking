cdb2c24bb114db14537be07c6f3b9b275261f555
Valentin Despa <info@vdespa.de>
2015-06-21 15:27:40 +0200

    Updated sql file

--- source/components/com_asinustimetracking/admin/sql/updates/mysql/1.2.0.sql
+++ source/components/com_asinustimetracking/admin/sql/updates/mysql/1.2.0.sql
@@ -1,6 +1,2 @@
 ALTER TABLE  `#__asinustimetracking_selection` ADD  `state` TINYINT(3) NOT NULL DEFAULT '1' AFTER `cg_id`;
-
-IF NOT EXISTS( (SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA=DATABASE()
-        AND COLUMN_NAME='employee_id' AND TABLE_NAME='#__asinustimetracking_user') ) THEN
-    ALTER TABLE  `#__asinustimetracking_user` ADD  `employee_id` INT( 11 ) NOT NULL;
-END IF;
+ALTER IGNORE TABLE  `#__asinustimetracking_user` ADD  `employee_id` INT( 11 ) NOT NULL;
