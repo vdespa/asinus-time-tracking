ba614ec14ea167b99c375d29103ed87f596ed295
Valentin Despa <info@vdespa.de>
2015-06-21 22:23:00 +0200

    SQL Updates

--- source/components/com_asinustimetracking/admin/sql/updates/mysql/1.2.0.sql
+++ source/components/com_asinustimetracking/admin/sql/updates/mysql/1.2.0.sql
@@ -3,12 +3,12 @@ ALTER TABLE  `#__asinustimetracking_selection` ADD  `state` TINYINT(3) NOT NULL 
 SET @s = (SELECT IF(
     (SELECT COUNT(*)
         FROM INFORMATION_SCHEMA.COLUMNS
-        WHERE table_name = 'table_name'
+        WHERE table_name = `#__asinustimetracking_user`
         AND table_schema = DATABASE()
-        AND column_name = 'col_name'
+        AND column_name = 'employee_id'
     ) > 0,
-    "SELECT 1",
-    "ALTER IGNORE TABLE `#__asinustimetracking_user` ADD  `employee_id` INT( 11 ) NOT NULL"
+    SELECT 1,
+    ALTER IGNORE TABLE `#__asinustimetracking_user` ADD `employee_id` INT( 11 ) NOT NULL
 ));
 
 PREPARE stmt FROM @s;
