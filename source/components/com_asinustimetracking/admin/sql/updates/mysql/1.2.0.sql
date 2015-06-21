97a5ff194e3443e3a2a4541037b269fdda7d320e
Valentin Despa <info@vdespa.de>
2015-06-21 15:12:51 +0200

    Updated sql file

--- source/components/com_asinustimetracking/admin/sql/updates/mysql/1.2.0.sql
+++ source/components/com_asinustimetracking/admin/sql/updates/mysql/1.2.0.sql
@@ -1,1 +1,8 @@
+-- Add a new column
 ALTER TABLE  `#__asinustimetracking_selection` ADD  `state` TINYINT(3) NOT NULL DEFAULT '1' AFTER `cg_id`;
+
+-- Add a column safely
+IF NOT EXISTS( (SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA=DATABASE()
+        AND COLUMN_NAME='employee_id' AND TABLE_NAME='#__asinustimetracking_user') ) THEN
+    ALTER TABLE  `#__asinustimetracking_user` ADD  `employee_id` INT( 11 ) NOT NULL;
+END IF;
